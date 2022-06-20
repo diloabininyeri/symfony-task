7b0e28036fc88826826e39fa7531e9c9c88708af
Dılo sürücü <berxudar@gmail.com>
2022-06-20 15:56:43 +0300

    removed test service lines

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,8 +15,6 @@ class HomeController extends AbstractController
     ]
     public function index(TestServiceInterface $testService): Response
     {
-
-        $testService->test();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
