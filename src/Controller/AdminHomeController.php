8393d2562b9749e0d03c9e3cf83da35b75c35236
Dılo sürücü <berxudar@gmail.com>
2022-06-22 23:57:02 +0300

    Implemented admin lte theme for admin panel basically

--- src/Controller/AdminHomeController.php
+++ src/Controller/AdminHomeController.php
@@ -6,19 +6,25 @@ use App\Traits\AbortIf;
 use App\Traits\CheckAdmin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ *
+ */
 class AdminHomeController extends AbstractController
 {
     use CheckAdmin, AbortIf;
 
-    #[Route('/admin/home', name: 'app_admin_home')]
+    /**
+     * @return Response
+     */
+    #[Route('/admin/', name: 'app_admin_home')]
     public function index(): Response
     {
-        $this->checkIsAdmin($this->getUser());
+        //@todo this line should be uncommented later
+//        $this->checkIsAdmin($this->getUser());
 
-        return $this->render('admin_home/index.html.twig', [
+        return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminHomeController',
         ]);
     }
