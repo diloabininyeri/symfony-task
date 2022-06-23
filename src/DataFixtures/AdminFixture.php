3268e9af3d8436da0d57b9aba5f580319b02b4b2
Dılo sürücü <berxudar@gmail.com>
2022-06-24 00:37:48 +0300

    AdminFixture has been created for admin user

---
+++ src/DataFixtures/AdminFixture.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\MigratingPasswordHasher;
+
+/**
+ *
+ */
+class AdminFixture extends Fixture
+{
+    /**
+     * you can create different password => symfony console security:hash-password admin
+     * @var string
+     */
+    private string $password = '$2y$13$tpL2cdzgJgKwwM/BA0X2bult51xIwx8SvwQ3cvi2lPEe6LksWGrke';  //admin
+
+    /**
+     * @param ObjectManager $manager
+     * @return void
+     */
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('admin@admin.com');
+        $user->giveRoleAdminPermission();
+        $user->setPassword($this->createHashPassword());
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+
+    /**
+     * @return string
+     */
+    private function createHashPassword(): string
+    {
+
+
+        return $this->password;
+    }
+}
