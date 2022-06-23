b40ac69ec4506183f424ceab7b303a09d7a9ca20
Dılo sürücü <berxudar@gmail.com>
2022-06-24 01:21:03 +0300

    all vehicles  have been listed for check the whether working eav model  too basically for now

--- src/Controller/Admin/VehicleController.php
+++ src/Controller/Admin/VehicleController.php
@@ -5,6 +5,8 @@ namespace App\Controller\Admin;
 
 namespace App\Controller\Admin;
 
+use App\Entity\Vehicle;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +14,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class VehicleController extends AbstractController
 {
     #[Route('/admin/vehicles', name: 'app_admin_vehicles')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
-        ]);
+
+        // @todo this should be paginate
+        $vehicles = $entityManager->getRepository(Vehicle::class)->findAll();
+        return $this->render(
+            'admin/vehicles.html.twig',
+            compact('vehicles')
+        );
     }
 }
