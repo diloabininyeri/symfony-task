0af1a6c0244e388abbaced77a7df6a0169d4890a
Dılo sürücü <berxudar@gmail.com>
2022-06-24 19:10:21 +0300

    category form submit and stored  has been completed ,form will be cleared

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\VehicleCategory;
 use App\Form\CategoryFormType;
 use App\Structure\Interfaces\CategoryServiceInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,13 +22,16 @@ class CategoryController extends AbstractController
         Route('/admin/category/create', name: 'app_admin_category_create'),
         IsGranted('ROLE_ADMIN')
     ]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(CategoryFormType::class);
+        $vehicleCategory = new VehicleCategory();
+        $form = $this->createForm(CategoryFormType::class, $vehicleCategory);
+        $this->handleForm($form, $request, $entityManager, $vehicleCategory);
 
         return $this->render(
             'admin/category/add_category.html.twig',
             [
+                'vehicle_category' => $vehicleCategory,
                 'form' => $form->createView()
             ]
         );
@@ -41,5 +48,23 @@ class CategoryController extends AbstractController
             'admin/category/list_category.html.twig',
             compact('categories')
         );
+    }
+
+    /**
+     * @param FormInterface $form
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param VehicleCategory $vehicleCategory
+     * @return VehicleCategory|null
+     */
+    public function handleForm(FormInterface $form, Request $request, EntityManagerInterface $entityManager, VehicleCategory $vehicleCategory): VehicleCategory|null
+    {
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $entityManager->persist($vehicleCategory);
+            $entityManager->flush();
+        }
+        return $vehicleCategory;
     }
 }
