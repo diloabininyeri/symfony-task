9c71bc02ac6df6b43b2aa0644e84756ba415e15b
Dılo sürücü <berxudar@gmail.com>
2022-06-24 16:30:54 +0300

    The category name has been made unique, so that a category can be created with the same name.

--- src/Entity/VehicleCategory.php
+++ src/Entity/VehicleCategory.php
@@ -15,7 +15,7 @@ class VehicleCategory
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255,unique: true)]
     private $name;
 
     #[ORM\Column(type: 'text')]
